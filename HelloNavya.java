c4f37b82bb3b84167adcb1c8012a588cbbf03541
Navya110305 <mnavya801@gmail.com>
2025-08-04 19:44:59 +0530

    Add files via upload

---
+++ HelloNavya.java
@@ -0,0 +1,11 @@
+public class HelloNavya {
+    public static void main(String[] args) {
+        System.out.println("Hello, Navya!");
+        
+        int a = 5;
+        int b = 7;
+        int sum = a + b;
+        
+        System.out.println("Sum of " + a + " and " + b + " is: " + sum);
+    }
+}
